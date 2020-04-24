6108f885dd0089279c625300879b0703ec138e84
Douglas Hanley <thesecretaryofwar@gmail.com>
2020-04-23 23:13:44 -0400

    daily data

--- code/plot_tools.py
+++ code/plot_tools.py
@@ -5,10 +5,19 @@ import matplotlib.pyplot as plt
 
 mpl.style.use('clean.mplstyle')
 
-def get_shifted(ser, start):
+eps = 1e-8
+
+def get_start(ser, start):
     pos = ser > start
     if pos.any():
-        return ser[pos.idxmax():]
+        return pos.idxmax()
+    else:
+        return None
+
+def get_shifted(ser, start):
+    date = get_start(ser, start)
+    if date is not None:
+        return ser[date:]
     else:
         return None
 
@@ -17,7 +26,11 @@ def get_aligned(sel, start):
     pos = pd.concat({c: s.reset_index(drop=True) for c, s in net.items() if s is not None}, axis=1)
     return pos
 
-def gen_ticks(ymin, ymax):
+def gen_ticks(ymin, ymax, per):
+    if ymin <= 0:
+        yield 0
+        ymin = 1/per
+
     ppow = np.floor(np.log10(ymin))
     pnum = ymin/np.power(10.0, ppow)
 
@@ -44,36 +57,38 @@ def gen_ticks(ymin, ymax):
 class FixedLogScale(mpl.scale.ScaleBase):
     name = 'fixed_log'
 
-    def __init__(self, axis, scale=1e6):
+    def __init__(self, axis, per=1e6):
         super().__init__(axis)
-        self.scale = scale
+        self.per = per
 
     def get_transform(self):
         return mpl.scale.FuncTransform(np.log, np.exp)
 
     def set_default_locators_and_formatters(self, axis):
         class InverseFormatter(mpl.ticker.Formatter):
-            def __init__(self, scale):
-                self.scale = scale
+            def __init__(self, per):
+                self.per = per
 
             def __call__(self, x, pos=None):
-                d = self.scale*x
-                if d >= 1:
-                    return '%d' % int(d)
+                d = self.per*x
+                if d == 0:
+                    return '0'
+                elif d >= 1:
+                    return '%d' % int(d+eps)
                 else:
                     return '%.1f' % d
 
         ymin, ymax = axis.get_view_interval()
-        ticks = list(gen_ticks(ymin, ymax))
+        ticks = list(gen_ticks(ymin, ymax, self.per))
         loc = mpl.ticker.FixedLocator(ticks)
 
         axis.set_major_locator(loc)
-        axis.set_major_formatter(InverseFormatter(self.scale))
+        axis.set_major_formatter(InverseFormatter(self.per))
         axis.set_minor_locator(mpl.ticker.NullLocator())
 
 mpl.scale.register_scale(FixedLogScale)
 
-def plot_progress(codes, names=None, data=None, figsize=(8, 5), xylabel=(6, -4)):
+def plot_progress(codes, names=None, data=None, figsize=(8, 5), xylabel=(6, -4), per=1e6):
     # get correct labels
     if names is None:
         names = {c: c for c in codes}
@@ -101,13 +116,13 @@ def plot_progress(codes, names=None, data=None, figsize=(8, 5), xylabel=(6, -4))
     ax.set_xlim(dmin-2, dmax+5)
 
     # set up axes
-    ax.set_yscale('fixed_log')
+    ax.set_yscale('fixed_log', per=per)
     ax.grid(axis='y', linewidth=1, alpha=0.3)
     ax.legend([])
 
     return fig, ax
 
-def plot_aligned(names, data=None, start=1e-6):
+def plot_aligned(names, data=None, diff=False, start=1e-6, per=1e6):
     if type(names) is list:
         names = {n: n for n in names}
     codes = list(names)
@@ -119,6 +134,9 @@ def plot_aligned(names, data=None, start=1e-6):
     else:
         align = data
 
-    fig, ax = plot_progress(codes, names=names, data=align)
+    if diff:
+        align = align.diff(axis=0).where(lambda x: x > 0)
+
+    fig, ax = plot_progress(codes, names=names, data=align, per=per)
 
     return fig, ax
